25e62c52e71ea99fe3fb4ed0dd030ce88ae38cee
Remington427 <94143909+Remington427@users.noreply.github.com>
2023-03-11 18:21:17 +0100

    Déplacement zombie

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -4,25 +4,76 @@ using UnityEngine;
 
 public class ZombieController : MonoBehaviour
 {
+    public Transform target;
+
+    UnityEngine.AI.NavMeshAgent navMeshAgent;
+
     Animator animator;
 
     const string STAND_STATE = "Stand";
     public const string DEFEATED_STATE = "Defeated";
+    public const string RUN_STATE = "Run";
 
     public string currentAction;
+
+    private float nextSearch;
+    private float searchRange = 2f;
+
+    private float startDecay;
+    private float decayTime = 10f;
 
     // Start is called before the first frame update
     void Awake()
     {
         currentAction = STAND_STATE;
         animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        nextSearch = 0.0f;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentAction == DEFEATED_STATE)
+        {
+            navMeshAgent.ResetPath();
+            if(Time.time > (startDecay + decayTime))
+            {
+                Decay();
+            }
+        }
+        else if(Time.time > nextSearch)
+        {
+            nextSearch = Time.time + searchRange;
+            GameObject[] joueurs = GameObject.FindGameObjectsWithTag("Player");
+            if(joueurs.Length != 0) target = GetClosestPlayer(joueurs);
+        }
+        else if(target != null)
+        {
+            if(MovingToTarget())
+            {
+                return;
+            }
+        }
+    }
+
+    private Transform GetClosestPlayer(GameObject[] joueurs)
+    {
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject joueur in joueurs)
+        {
+            Vector3 diff = joueur.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = joueur;
+                distance = curDistance;
+            }
+        }
+        return closest.transform;
     }
 
     private void Stand()
@@ -37,11 +88,48 @@ public class ZombieController : MonoBehaviour
         ResetAnimation();
         currentAction = DEFEATED_STATE;
         animator.SetBool(DEFEATED_STATE, true);
+        startDecay = Time.time;
+    }
+
+    private void Run()
+    {
+        ResetAnimation();
+        currentAction = RUN_STATE;
+        animator.SetBool(RUN_STATE, true);
     }
 
     private void ResetAnimation()
     {
         animator.SetBool(STAND_STATE, false);
         animator.SetBool(DEFEATED_STATE, false);
+        animator.SetBool(RUN_STATE, false);
+    }
+
+    private bool MovingToTarget()
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+
+        if(navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance)
+        {
+            if(currentAction != RUN_STATE) Run();
+        }
+        else
+        {
+            RotateToTarget();
+            return false;
+        }
+        return true;
+    }
+
+    private void RotateToTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 3f);
+    }
+
+    private void Decay()
+    {
+        Destroy(gameObject);
     }
 }
